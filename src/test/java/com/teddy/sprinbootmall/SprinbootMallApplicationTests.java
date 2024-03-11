23c710bce24485f20fd35cc993872bbf39b42c92
Teddy <teddy13643@gmail.com>
2024-03-11 16:57:21 +0800

    first add

---
+++ src/test/java/com/teddy/sprinbootmall/SprinbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teddy.sprinbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinbootMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
